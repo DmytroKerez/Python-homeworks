74cd851bd8ae970755f46bd2b3b3ca0b20df838e
Dmytro Kerez <dmytro.kerez@gmail.com>
2022-02-20 21:05:56 +0200

    task2 added

---
+++ homeworks/17_Feb/task2.py
@@ -0,0 +1,10 @@
+name = input("What is your name? ")
+age = int(input("Okay, nice name. What about age? How old are you? "))
+print(f'Hello {name.capitalize()}, on your next birthday you’ll be {age+1} years.')
+
+
+
+
+
+
+
