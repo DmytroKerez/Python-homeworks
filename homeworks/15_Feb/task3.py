2eebbaa6b104da542870d984e057d9f5f7b324f1
Dmytro Kerez <dmytro.kerez@gmail.com>
2022-02-20 20:43:17 +0200

    task3 15_Feb

---
+++ homeworks/15_Feb/task3.py
@@ -0,0 +1,11 @@
+while True: 
+    name = "dmytro"
+    a = input("Enter a name: ") 
+    if a.lower() == name:
+        print(True)
+        break
+    else:
+        print(False)
+        continue
+    
+    
